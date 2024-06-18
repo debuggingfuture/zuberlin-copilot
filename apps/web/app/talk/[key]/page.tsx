3664b7062c13e80db6bd7cce215fab6dd6f41da2
debuggingfuture (Vincent LCY) <lauchunyin@debuggingfuture.com>
2024-06-18 18:29:36 +0800

    add: wallet (#1)

--- apps/web/app/talk/[key]/page.tsx
+++ apps/web/app/talk/[key]/page.tsx
@@ -1,22 +1,31 @@
 // hard code wallet for demo
 'use client';
+import { useAuthCore, useConnect } from "@particle-network/auth-core-modal";
 import "../../globals.css";
-
-const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || ''
-
-// import {
-//   PublicClient,
-//   createWalletClient,
-//   createPublicClient,
-//   defineChain,
-//   getContract,
-//   http,
-//   WalletClient,
-// } from 'viem';
-// import { privateKeyToAccount } from 'viem/accounts';
-// import { mantleSepoliaTestnet } from 'viem/chains';
+import { AuthType } from "@particle-network/auth-core";
+import { useState } from "react";
 
 export default function Page(): JSX.Element {
+
+  // Particle Auth Core via useConnect
+  const { connect, connected } = useConnect();
+
+  const { userInfo } = useAuthCore();
+
+  const [voteByNoteId, setVoteById] = useState<Record<string, any>>({})
+
+  const handleLogin = async (authType: any) => {
+    if (!connected) {
+      // if (!userInfo) {
+      await connect({
+        // socialType: authType,
+      });
+      // }
+    }
+
+  };
+
+  const knowledgeLevel = 3;
 
   // const config = {
   //   chain: mantleSepoliaTestnet,
@@ -29,8 +38,21 @@ export default function Page(): JSX.Element {
   // });
 
 
+  const save = async () => {
+    // abi write
+  }
+
 
   const vote = async (noteId: string, isHelpful: boolean) => {
+
+    console.log('userInfo', userInfo);
+    setVoteById({
+      ...voteByNoteId,
+      [noteId]: {
+        isHelpful,
+        knowledgeLevel,
+      }
+    })
     // TODO move to NFT
 
     // // TODO update deployed address
@@ -84,70 +106,99 @@ export default function Page(): JSX.Element {
     }
   ]
 
+  const findBtnClass = (noteId: string, isCheckHelpful: boolean) => {
+    if (voteByNoteId[noteId]?.isHelpful === true && isCheckHelpful
+      ||
+      !voteByNoteId[noteId]?.isHelpful === false && !isCheckHelpful
+    ) {
+      return 'btn btn-outline text-white btn-active';
+    }
+    return 'btn btn-outline text-white';
+  }
+
   return (
-    <main className="font-mono container w-3/4 bg-blue m-20">
-      <div>
-        <h2 className="text-4xl pb-2">
-          {talkDetails.title}
-        </h2>
-      </div>
-      <div className="text-lg">
-        <h3>
-          {talkDetails.speaker}
-        </h3>
-      </div>
-      <div className="text-sm pt-8">
-        {talkDetails.description}
-      </div>
-      <div className="text-2xl pt-10">
+    <>
+      <nav className="p-2">
+        {
+          !connected && (
+            <div>
+              <button className="btn" onClick={() => {
+                handleLogin('email')
+              }}>Connect</button>
+            </div>
+          )
+        }
 
-        <h3>Community Notes</h3>
 
-      </div>
-      <div>
-        <div className="flex flex-col text-lg ">
-          {
-            cards.map(
-              card => (
-                <div className="rounded-lg bg-slate-600 m-5 p-5">
-                  <h5 className="text-xl font-bold">{card.title}</h5>
-                  <p>{card.description}</p>
-                  {card.referenceUrls?.length && (
-                    card.referenceUrls.map(
-                      url => (
-                        <div>
-                          <a className="bg-blue-500" href={url}>reference
-                          </a>
-                        </div>
-                      )
-                    )
-                  )}
-                  <div className="flex flex-row">
-                    <div className="relative mr-5"><button onClick={() => vote(card.id, true)}
-                      className="btn btn-outline text-white">✅ Helpful</button></div>
-                    <div className="relative ml-5"><button onClick={() => vote(card.id, false)}
-                      className="btn btn-outline text-white">✖️ Not Helpful</button></div>
-                  </div>
-                </div>
-              )
-            )
-          }
+      </nav>
+      <main className="font-mono container w-3/4 bg-blue m-20">
+
+        <div>
+          <h2 className="text-4xl pb-2">
+            {talkDetails.title}
+          </h2>
+        </div>
+        <div className="text-lg">
+          <h3>
+            {talkDetails.speaker}
+          </h3>
+        </div>
+        <div className="text-sm pt-8">
+          {talkDetails.description}
+        </div>
+        <div className="text-2xl pt-10">
+
+          <h3>Community Notes</h3>
 
 
         </div>
-      </div>
-
-
-
-      <div className="p-5">
-        <h3 className="text-lg">Create a Note for the community</h3>
-
-        <textarea id="suggestion-textarea" placeholder="Add Note" className="textarea textarea-bordered textarea-lg w-11/12 p-5" ></textarea>
         <div id="submit-button"
-          className="flex flex-row-reverse w-full"><button className="btn btn-outline text-white text-xl">Submit</button></div>
-      </div >
+          className="flex flex-row-reverse w-full">
+          <button onClick={() => { save(); }} className="btn btn-outline text-white text-xl">Save</button></div>
+        <div>
+          <div className="flex flex-col text-lg ">
+            {
+              cards.map(
+                card => (
+                  <div className="rounded-lg bg-slate-600 m-5 p-5">
+                    <h5 className="text-xl font-bold">{card.title}</h5>
+                    <p>{card.description}</p>
+                    {card.referenceUrls?.length && (
+                      card.referenceUrls.map(
+                        url => (
+                          <div>
+                            <a className="bg-blue-500" href={url}>reference
+                            </a>
+                          </div>
+                        )
+                      )
+                    )}
+                    <div className="flex flex-row">
+                      <div className="relative mr-5"><button onClick={() => vote(card.id, true)}
+                        className={findBtnClass(card.id, true)}>✅ Helpful</button></div>
+                      <div className="relative ml-5"><button onClick={() => vote(card.id, false)}
+                        className={findBtnClass(card.id, false)}>✖️ Not Helpful</button></div>
+                    </div>
+                  </div>
+                )
+              )
+            }
 
-    </main>
 
+          </div>
+        </div>
+
+
+
+        <div className="p-5">
+          <h3 className="text-lg">Create a Note for the community</h3>
+
+          <textarea id="suggestion-textarea" placeholder="Add Note" className="textarea textarea-bordered textarea-lg w-11/12 p-5" ></textarea>
+          <div id="submit-button"
+            className="flex flex-row-reverse w-full"><button className="btn btn-outline text-white text-xl">Submit</button></div>
+        </div >
+
+      </main >
+    </>
   )
 }
